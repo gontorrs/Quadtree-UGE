c92ea974120033500044e0f6e51bdbadfc4d29ea
MarioMaison <ken.marc83@gmail.com>
2024-12-05 18:17:25 +0100

    Added function to create pixmap + quadtree

--- Code/quad.c
+++ Code/quad.c
@@ -1,35 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef unsigned char graylvl;
-
-typedef struct erruni{
-    int e : 
-}erruni;
+#include <string.h>
+#include <math.h>
 
 typedef struct Pixnode{
-    graylvl m;
-    int n;
-    int u;
+    unsigned char m;
+    unsigned char e : 2;
+    unsigned char u : 1;
 }Pixnode;
 
 typedef struct Quadtree{
     Pixnode* Pixels;
     int treesize;
+    int levels;
 }Quadtree;
 
-int treelevel(int n){
-    int res;
-    int pow = 2;
-    for(int i = 1; i<12;i++){
-        pow = pow*i;
-        if(pow == n){
-            res = i+1;
-            return res;
-        }
+
+// Function to read a PGM file and return pixel data
+unsigned char* readPGM(const char* filename, int* width, int* height, int* maxGray) {
+    FILE* file = fopen(filename, "rb");
+    if (!file) {
+        perror("Error opening file");
+        return NULL;
     }
+
+    char magicNumber[3];
+    fscanf(file, "%2s", magicNumber);
+    if (strcmp(magicNumber, "P5") != 0) {
+        fprintf(stderr, "Unsupported format: %s (only binary PGM supported)\n", magicNumber);
+        fclose(file);
+        return NULL;
+    }
+
+    // Skip comments
+    int c;
+    while ((c = fgetc(file)) == '#') {
+        while (fgetc(file) != '\n');
+    }
+    ungetc(c, file);
+
+    // Read image dimensions and max gray level
+    fscanf(file, "%d %d %d", width, height, maxGray);
+    if (*width != *height || (*width & (*width - 1)) != 0) {
+        fprintf(stderr, "Error: Only square images of size 2^n x 2^n are supported.\n");
+        fclose(file);
+        return NULL;
+    }
+
+    // Read pixel data
+    fgetc(file); 
+    size_t dataSize = (*width) * (*height);
+    unsigned char* data = (unsigned char*)malloc(dataSize);
+    if (!data) {
+        perror("Error allocating memory");
+        fclose(file);
+        return NULL;
+    }
+
+    fread(data, 1, dataSize, file);
+    fclose(file);
+
+    return data;
 }
 
-Quadtree* init_quadtree(int picture_size){
+// Function to initialize the QuadTree
+Quadtree* initializeQuadtree(int levels) {
+    if (levels <= 0) {
+        printf("Error: Levels must be greater than 0.\n");
+        return NULL;
+    }
 
+    // Allocate memory for the QuadTree
+    Quadtree* tree = (Quadtree*)malloc(sizeof(Quadtree));
+    if (!tree) {
+        printf("Error: Memory allocation failed for QuadTree.\n");
+        return NULL;
+    }
+
+    // Calculate the tree size
+    tree->levels = levels;
+    tree->treesize = calculateTreeSize(levels);
+
+    // Allocate memory for the pixels
+    tree->Pixels = (Pixnode*)malloc(tree->treesize * sizeof(Pixnode));
+    if (!tree->Pixels) {
+        printf("Error: Memory allocation failed for pixels.\n");
+        free(tree);
+        return NULL;
+    }
+
+    // Initialize pixels to default values
+    for (int i = 0; i < tree->treesize; i++) {
+        tree->Pixels[i].m = 0; 
+        tree->Pixels[i].u = 1;
+        tree->Pixels[i].e = 0; 
+    }
+
+    return tree;
+}
+
+
+int main(int argc, char **argv){
+    if (argc!= 3) {
+        fprintf(stderr, "Usage: %s <input_pgm_file> <output_quadtree_file>\n", argv[0]);
+        return 1;
+    }
+
+    const char* inputFilename = argv[1];
+    const char* outputFilename = argv[2];
+
+    int width, height, maxGray;
+    unsigned char* pixmap = readPGM(inputFilename, &width, &height, &maxGray);
+    if (!pixmap) {
+        return 1;
+    }
+
+    int levels = log2(width) + 1;
+
+    // TODO: Implement Quadtree compression and write to output file
+
+    free(pixmap);
+    return 0;
 }
